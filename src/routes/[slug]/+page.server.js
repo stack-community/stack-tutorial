20e0548e4223a861c99bbe4a2cc6d5b931402af7
Osamu Miyazawa <miyazawa.o.aa@m.titech.ac.jp>
2024-02-09 22:02:39 +0900

    文章とStackのコードを、contents/aritcleに配置したファイルから読み込むように変更。

--- src/routes/[slug]/+page.server.js
+++ src/routes/[slug]/+page.server.js
@@ -1,10 +1,28 @@
 import { error } from '@sveltejs/kit';
 import { articles } from '../data.js';
+import fs from 'fs';
 
 export function load({ params }) {
-    const article = articles.find((a) => a.slug === params.slug);
+    const filePath = `contents/article/${params.slug}`;
 
-    if (!article) throw error(404);
+    const markdownFileName = fs.readdirSync(filePath).find(fileName => fileName.endsWith('.md'));
+    const codeFileName = fs.readdirSync(filePath).find(fileName => fileName.endsWith('.stk'));
+    if (!markdownFileName || !codeFileName) throw error(404);
+
+    const markdown = fs.readFileSync(`${filePath}/${markdownFileName}`);
+    const code = fs.readFileSync(`${filePath}/${codeFileName}`);
+    if (!markdown || !code) throw error(404);
+
+    let a = articles.find((a) => a.slug === params.slug);
+    if (!a) throw error(404);
+
+    const article = {
+        slug: a.slug,
+        title: a.title,
+        content: markdown.toString(),
+        code: code.toString(),
+        next: a.next
+    };
 
     return {
         article
